26ce4996a894af074ffa3e7dd1b2450533d81536
elenmeadows <elenmeadows@gmail.com>
2022-01-08 03:01:04 +0300

    07 jan 2022

--- js/main.js
+++ js/main.js
@@ -1,14 +1,82 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-  document.getElementById("mySidenav").style.height = "fit-content";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+$(".set-icon").click(function() {
+  if ( $( ".settings" ).is( ":hidden" ) ) {
+    $(".settings").slideDown(100);
+    $(".settings").css("display", "flex");
+    $("body").css("background-color", "rgba(0,0,0,0.6)");
+    $("body").css("z-index", 1000);
+    $(".main-nav").css("pointer-events", "none");
+    $(".content-window").css("pointer-events", "none");
+  } else {
+    $(".settings").slideUp(200);
+    $("body").css("background-color", "white");
+    $(".main-nav").css("pointer-events", "");
+    $(".content-window").css("pointer-events", "");
+  }
+});
+
+window.onclick = function(event) {
+  if (!event.target.matches(".set-icon", ".settings", ".settings a") && !event.target.matches(".settings") && !event.target.matches(".settings a")) {
+      $(".settings").slideUp(200);
+      $("body").css("background-color", "white");
+      $(".main-nav").css("pointer-events", "");
+      $(".content-window").css("pointer-events", "");
+  }
+};
+
+function mQuery() {
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+        $(".settings").slideUp(1);
+        $("body").css("background-color", "white");
+        $(".main-nav").css("pointer-events", "");
+        $(".content-window").css("pointer-events", "");
+    }
+};
+
+mQuery();
+window.matchMedia("(min-width: 1024px)").addListener(mQuery);
+
+
+/* function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-  document.getElementById("mySidenav").style.height = "0";
-  document.body.style.backgroundColor = "white";
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} */
+
+
+/* function myFunction() {
+  document.querySelector(".settings").classList.toggle("show-settings");
+  let openedDropdown =
+  if () {
+
+  }
 }
+
+window.onclick = function(event) {
+  if (!event.target.matches('.set-icon')) {
+    let dropdowns = document.getElementsByClassName("settings");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show-settings')) {
+        openDropdown.classList.remove('show-settings');
+      }
+    }
+  }
+}
+*/
 
 /* let toggleNavStatus = false;
 
@@ -22,6 +90,7 @@ let toggleNav = function() {
         getSidebarUl.style.visibility = "visible";
         getSidebar.style.width = "290px";
         getSidebarTitle.style.opacity = "0.5";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 
         let arrayLength = getSidebarLinks.length;
         for (let i = 0; i < arrayLength; i++) {
@@ -32,9 +101,9 @@ let toggleNav = function() {
     }
 
     else if (toggleNavStatus === true) {
-        getSidebarUl.style.visibility = "visible";
         getSidebar.style.width = "60px";
         getSidebarTitle.style.opacity = "0";
+        document.body.style.backgroundColor = "white";
 
         let arrayLength = getSidebarLinks.length;
         for (let i = 0; i < arrayLength; i++) {
@@ -45,5 +114,4 @@ let toggleNav = function() {
 
         toggleNavStatus = false;
     }
-}
-*/
+} */
