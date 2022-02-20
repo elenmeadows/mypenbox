50f6aff64744c383e5e9e4e00cf4a56f4c19c1a0
Helena Meadows <elenmeadows@gmail.com>
2022-02-20 21:53:10 +0300

    catalog table (frontend) + scrollbar + black background (settings part)

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
 $(".settings").css("display", "flex").hide();
 
-$(".set-icon").click(function() {
-  if ( $( ".settings" ).is( ":hidden" ) ) {
+$(".set-icon").click(function () {
+  if ($(".settings").is(":hidden")) {
     $(".settings").slideDown(700);
-    $("body").css("background-color", "rgba(0,0,0,0.7)");
+    $(".settings-background").css("display", "block");
     $(".main-nav").css("pointer-events", "none");
     $(".content-window").css("pointer-events", "none");
     $("header").css("box-shadow", "0 0.35rem 0.35rem rgba(60, 10, 41, .7)");
   } else {
     $(".settings").slideUp(200);
-    $("body").css("background-color", "white");
+    $(".settings-background").css("display", "none");
     $(".main-nav").css("pointer-events", "");
     $(".content-window").css("pointer-events", "");
     $("header").css("box-shadow", "");
   }
 });
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches(".set-icon") && !event.target.matches(".settings") && !event.target.matches(".settings a")) {
-      $(".settings").slideUp(200);
-      $("body").css("background-color", "white");
-      $(".main-nav").css("pointer-events", "");
-      $(".content-window").css("pointer-events", "");
-      $("header").css("box-shadow", "");
+    $(".settings").slideUp(200);
+    $(".settings-background").css("display", "none");
+    $(".main-nav").css("pointer-events", "");
+    $(".content-window").css("pointer-events", "");
+    $("header").css("box-shadow", "");
   }
 };
 
 function mQuery() {
-    if (window.matchMedia("(min-width: 1024px)").matches) {
-        $(".settings").slideUp(1);
-        $("body").css("background-color", "white");
-        $(".main-nav").css("pointer-events", "");
-        $(".content-window").css("pointer-events", "");
-        $("header").css("box-shadow", "");
-    }
+  if (window.matchMedia("(min-width: 1024px)").matches) {
+    $(".settings").slideUp(1);
+    $(".settings-background").css("display", "none");
+    $(".main-nav").css("pointer-events", "");
+    $(".content-window").css("pointer-events", "");
+    $("header").css("box-shadow", "");
+  }
 };
 
 mQuery();
