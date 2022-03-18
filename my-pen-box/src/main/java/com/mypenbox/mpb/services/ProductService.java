5968055d42db820f05e1514de729830d3b109cb5
Helena Meadows <elenmeadows@gmail.com>
2022-03-19 01:39:36 +0300

    basic front-end of sidebar for laptop -> done

--- my-pen-box/src/main/java/com/mypenbox/mpb/services/ProductService.java
+++ my-pen-box/src/main/java/com/mypenbox/mpb/services/ProductService.java
@@ -2,33 +2,30 @@ package com.mypenbox.mpb.services;
 
 import com.mypenbox.mpb.models.Product;
 import com.mypenbox.mpb.repo.ProductRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@Slf4j
-@RequiredArgsConstructor
 public class ProductService {
-    private final ProductRepository productRepository;
 
-    public List<Product> listProducts(String colorname) {
-        if (colorname != null) return productRepository.findByColorname(colorname);
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> listAll() {
         return productRepository.findAll();
     }
 
     public void saveProduct(Product product) {
-        log.info("Saving new {}", product);
         productRepository.save(product);
+    }
+
+    public Product getProduct(Long id) {
+        return productRepository.findById(id).get();
     }
 
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
-    }
-
-    public Product getProductById(Long id) {
-        return productRepository.findById(id).orElse(null);
     }
 }
