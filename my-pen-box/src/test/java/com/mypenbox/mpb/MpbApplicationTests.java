990133a2d280c97206b472f74b4a347cae7357c4
elenmeadows <elenmeadows@gmail.com>
2022-04-30 02:32:40 +0300

    amen

--- my-pen-box/src/test/java/com/mypenbox/mpb/MpbApplicationTests.java
+++ my-pen-box/src/test/java/com/mypenbox/mpb/MpbApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mypenbox.mpb;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MpbApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.mypenbox.mpb;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MpbApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
