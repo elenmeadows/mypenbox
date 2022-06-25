47569f240641e3a67b0c12b789bb6ec3e60b9364
Helena Meadows <elenmeadows@gmail.com>
2022-06-26 01:54:50 +0300

    thymeleaf sorting -> done

--- my-pen-box/src/main/java/com/mypenbox/mpb/controllers/AppController.java
+++ my-pen-box/src/main/java/com/mypenbox/mpb/controllers/AppController.java
@@ -4,6 +4,7 @@ import com.mypenbox.mpb.models.Product;
 import com.mypenbox.mpb.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,20 +20,27 @@ public class AppController {
 
     @RequestMapping("/catalog")
     public String viewCatalog(Model model) {
-        return viewPage(model, 1);
+        return viewPage(model, 1, "colormark", "asc");
     }
 
     @RequestMapping("/catalog/page/{pageNum}")
     public String viewPage(Model model,
-                              @PathVariable(name = "pageNum") int pageNum) {
+                           @PathVariable(name = "pageNum") int pageNum,
+                           @Param("sortField") String sortField,
+                           @Param("sortDir") String sortDir) {
 
-        Page<Product> page = service.listAll(pageNum);
+        Page<Product> page = service.listAll(pageNum, sortField, sortDir);
 
         List<Product> listProducts = page.getContent();
 
         model.addAttribute("currentPage", pageNum);
         model.addAttribute("totalPages", page.getTotalPages());
         model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
         model.addAttribute("listProducts", listProducts);
 
         return "catalog";
--- my-pen-box/src/main/java/com/mypenbox/mpb/services/ProductService.java
+++ my-pen-box/src/main/java/com/mypenbox/mpb/services/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +19,12 @@ public class ProductService {
     @Autowired
     private ProductRepository repo;
 
-    public Page<Product> listAll(int pageNum) {
+    public Page<Product> listAll(int pageNum, String sortField, String sortDir) {
 
-        int pageSize = 5;
-        Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
+        int pageSize = 8;
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize,
+                sortDir.equals("asc") ? Sort.by(sortField).ascending()
+                        : Sort.by(sortField).descending());
 
         return repo.findAll(pageable);
     }
