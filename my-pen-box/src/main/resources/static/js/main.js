2e86f5dff66622b78e239fa9e3d4125bc0ee2206
Helena Meadows <elenmeadows@gmail.com>
2022-07-04 17:40:18 +0300

    multselection animation -> done

--- my-pen-box/src/main/resources/static/js/main.js
+++ my-pen-box/src/main/resources/static/js/main.js
@@ -1,27 +1,7 @@
 
-// SettingsBar animation (mobile view)
+// Close menus if clicked outside
 
-$(".settings").css("display", "flex").hide();
-
-$(".settings-icon").click(function () {
-    if ($(".settings").is(":hidden")) {
-        $(".settings").slideDown(700);
-        $(".settings-icon").addClass("open");
-        $(".settings-background").css("display", "block");
-        $(".main-nav").css("pointer-events", "none");
-        $(".content-window").css("pointer-events", "none");
-        $("header").css("box-shadow", "0 0.35rem 0.35rem rgba(38, 36, 36, .7)");
-    } else {
-        $(".settings").slideUp(200);
-        $(".settings-icon").removeClass("open");
-        $(".settings-background").css("display", "none");
-        $(".main-nav").css("pointer-events", "");
-        $(".content-window").css("pointer-events", "");
-        $("header").css("box-shadow", "");
-    }
-});
-
-function settings() {
+function closeIfOutside() {
     window.onclick = function (event) {
         if (!event.target.matches(".settings-icon") && !event.target.matches(".settings-icon__burger") && !event.target.matches(".settings") && !event.target.matches(".settings *")) {
             $(".settings").slideUp(200);
@@ -40,7 +20,7 @@ function settings() {
     }
 };
 
-settings();
+closeIfOutside();
 
 // MediaQuery animations
 
@@ -68,6 +48,43 @@ mQuery();
 window.matchMedia("(min-width: 1024px)").addListener(mQuery);
 window.matchMedia("(min-width: 692px)").addListener(mQuery);
 window.matchMedia("(max-width: 692px)").addListener(mQuery);
+
+// SettingsBar animation (mobile view)
+
+$(".settings").css("display", "flex").hide();
+
+$(".settings-icon").click(function () {
+    if ($(".settings").is(":hidden")) {
+        $(".settings").slideDown(700);
+        $(".settings-icon").addClass("open");
+        $(".settings-background").css("display", "block");
+        $(".main-nav").css("pointer-events", "none");
+        $(".content-window").css("pointer-events", "none");
+        $("header").css("box-shadow", "0 0.35rem 0.35rem rgba(38, 36, 36, .7)");
+    } else {
+        $(".settings").slideUp(200);
+        $(".settings-icon").removeClass("open");
+        $(".settings-background").css("display", "none");
+        $(".main-nav").css("pointer-events", "");
+        $(".content-window").css("pointer-events", "");
+        $("header").css("box-shadow", "");
+    }
+});
+
+// SearchBar animation
+
+input.oninput = function () {
+    let searchInput = $(".search-input").val();
+    if (!searchInput == "") {
+        $(".reset-icon").css("display", "inline-block");
+    } else if (searchInput == "") {
+        $(".reset-icon").css("display", "none");
+    }
+}
+
+$(".reset-icon").click(function () {
+    $(".reset-icon").css("display", "none");
+});
 
 // SortByFilterMenu animation
 
@@ -97,19 +114,28 @@ $("td").click(function () {
     }
 });
 
-// SearchBar animation
+// Multiselection animation
 
-input.oninput = function () {
-    let searchInput = $(".search-input").val();
-    if (!searchInput == "") {
-        $(".reset-icon").css("display", "inline-block");
-    } else if (searchInput == "") {
-        $(".reset-icon").css("display", "none");
+$('#select-all').click(function(){
+    $('table input:checkbox').prop('checked', true);
+    $('.multselection__counter span').html($('table input:checkbox:checked').length);
+});
+
+$('#remove-selection').click(function(){
+    $('table input:checkbox').prop('checked', false);
+    $('.multselection__counter span').html($('table input:checkbox:checked').length);
+    $(".multselection").slideUp(400);
+});
+
+$(".multselection").css("display", "flex").hide();
+
+$("table input:checkbox").click(function() {
+    if ($('table input:checkbox:checked').length >= 1) {
+        $(".multselection").slideDown(400);
+    } else if ($('table input:checkbox:checked').length <= 2) {
+        $(".multselection").slideUp(400);
     }
-}
-
-$(".reset-icon").click(function () {
-    $(".reset-icon").css("display", "none");
+    $('.multselection__counter span').html($('table input:checkbox:checked').length);
 });
 
 // Modal window pop-up/collapse on click
